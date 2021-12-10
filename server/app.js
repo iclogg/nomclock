7441f433feff409d85edc1c7a841656a6472c0fd
Ida Loggert <ida.loggert@gmail.com>
2021-12-10 15:45:46 +0100

    install express, create app.js and start work on server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/test");
+app.get("/test");
+
+// set port and start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+});
