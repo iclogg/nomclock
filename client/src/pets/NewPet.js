d298f92b1a936f28ce630820112b8afcdc0b18da
Ida Loggert <ida.loggert@gmail.com>
2021-12-14 11:56:11 +0100

    start work to add form logit for multiple inputs

--- client/src/pets/NewPet.js
+++ client/src/pets/NewPet.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 
 import Input from "../shared/Input";
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from "../utils/validators";
+
+import { formReducer } from "../utils/form-hooks";
 
 const NewPet = () => {
+    const [formState, dispatch] = useReducer(formReducer, {
+        inputs: {
+            title: {
+                value: "",
+                isValid: false,
+            },
+            description: {
+                value: "",
+                isValid: false,
+            },
+        },
+        isValid: false,
+    });
+
+    const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({ type: "INPUT_CHANGE", value, isValid, inputId: id });
+    }, []);
+
     return (
         <div>
             <h2>Add your darling pet!</h2>
             <form action="">
                 <Input
+                    id="name"
                     element="input"
                     type="text"
-                    label="Name" /* validators={[]} onChange={} */
+                    label="Name"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a valid title."
+                    onChange={inputHandler}
+                />
+                <Input
+                    id="description"
+                    element="textarea"
+                    label="description"
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    errorText="Please enter a description (at least 5 characters)"
+                    onChange={inputHandler}
                 />
             </form>
         </div>
--- client/src/shared/Input.js
+++ client/src/shared/Input.js
@@ -1,18 +1,61 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
+
+import { validate } from "../utils/validators";
+
+//reducer
+
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case "CHANGE":
+            return {
+                ...state,
+                value: action.val,
+                isValid: validate(action.val, action.validators),
+            };
+        default:
+            return state;
+    }
+};
+
+//component
 
 const Input = (props) => {
-    useReducer();
+    const [inputState, dispatch] = useReducer(inputReducer, {
+        value: "",
+        isValid: false,
+    });
 
-    const changeHandler = (e) => {};
+    const { id, onInput } = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+        onInput(id, value, isValid);
+    }, [id, value, isValid, onInput]);
+
+    const changeHandler = (e) => {
+        dispatch({
+            type: "CHANGE",
+            val: e.target.value,
+            validators: props.validators,
+        });
+    };
+
+    /* TODO logic or library for not showinr error at start of input. One possibility is use onBlur and touchHandler */
 
     const element =
         props.element === "input" ? (
-            <input id={props.id} type={props.type} onChange={changeHandler} />
+            <input
+                id={props.id}
+                type={props.type}
+                onChange={changeHandler}
+                value={inputState.value}
+            />
         ) : (
             <textarea
                 id={props.id}
                 rows={props.rows || 3}
                 onChange={changeHandler}
+                value={inputState.value}
             />
         );
 
@@ -20,6 +63,7 @@ const Input = (props) => {
         <div>
             <label htmlFor={props.id}>{props.label}</label>
             {element}
+            {!inputState.isValid && <p>{props.errorText}</p>}
         </div>
     );
 };
