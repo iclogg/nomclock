685808c5e496d200ff16059ef3519118b8b03e57
Ida Loggert <ida.loggert@gmail.com>
2021-12-15 16:26:57 +0100

    add basic error handling middleware

--- server/app.js
+++ server/app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 
+const petRoutes = require("./routes/pet-routes");
+
 const app = express();
 
+// Middleware
 app.use(express.json());
 
-app.post("/test");
-app.get("/test");
+// Routes
+app.use("/api/pets", petRoutes);
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.log("in error handler");
+
+    if (res.headerSent) {
+        return next(err);
+    }
+
+    res.status(err.code || 500);
+    res.json({ message: err.message || "An unknow error happened." });
+});
 
 // set port and start server
 const port = process.env.PORT || 5000;
--- server/routes/pet-routes.js
+++ server/routes/pet-routes.js
@@ -2,7 +2,37 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-    console.log("get request in pet routes");
-    res.json({ message: "This is a lovely pet!" });
+/* TODO remove dummy pet once backen is up */
+const PETS = [
+    {
+        id: "pet1",
+        name: "Lucifer",
+        image: "/lucifer.png",
+        description: "Best kitten EVER!!!!",
+        maxMeals: 3,
+    },
+    {
+        id: "pet2",
+        name: "Mumman",
+        image: "/lucifer.png",
+        description: "Absolutely most superior kitten EVER!!!!",
+        maxMeals: 6,
+    },
+];
+
+router.get("/:petId", (req, res, next) => {
+    const petId = req.params.petId;
+    const pet = PETS.find((p) => {
+        return p.id === petId;
+    });
+    if (!pet) {
+        const err = new Error("Could not find that pet!");
+        err.code = 404;
+        return next(err);
+    }
+    console.log("after if");
+
+    res.json({ pet });
 });
+
+module.exports = router;
