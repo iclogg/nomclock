32d4f02a7cdd924999885f349a82e6c8fa4147d8
Ida Loggert <ida.loggert@gmail.com>
2021-12-18 11:38:44 +0100

    start work on user controllers

---
+++ server/controllers/user-controllers.js
@@ -0,0 +1,19 @@
+const HttpError = require("../models/http-error");
+
+let USERS = [
+    {
+        id: "u1",
+        name: "Julismund",
+        email: "julismund@notanemail.com",
+        password: "pass",
+        pets: ["pet1"],
+    },
+    {
+        name: "Mundus",
+        email: "mundus@notanemail.com",
+        password: "pass",
+        pets: ["pet2"],
+    },
+];
+
+let idCount = 2;
