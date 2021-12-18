f796ba250f01cdec043e08a97ad14033e77c6133
Ida Loggert <ida.loggert@gmail.com>
2021-12-18 17:00:41 +0100

    add create user and find user by it controller

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -1,19 +1,52 @@
 const HttpError = require("../models/http-error");
 
+/* TODO remove dummy user once backen is up */
+
 let USERS = [
     {
         id: "u1",
         name: "Julismund",
         email: "julismund@notanemail.com",
-        password: "pass",
         pets: ["pet1"],
     },
     {
         name: "Mundus",
         email: "mundus@notanemail.com",
-        password: "pass",
         pets: ["pet2"],
     },
 ];
 
 let idCount = 2;
+
+// TODO add package (or use mongodb? to create unique ids) uuid v4 perhaps?
+/* READ */
+
+const getUserById = (req, res, next) => {
+    const userId = req.params.userId;
+    const user = USER.find((u) => u.id === userId);
+
+    if (!user) {
+        return next(new HttpError("No user found.", 404));
+    }
+
+    res.status(200).json(user);
+};
+
+/* CREATE */
+
+const createUser = (req, res, next) => {
+    idCount += 1;
+    const { name, email } = req.body;
+
+    const createdUser = {
+        id: idCount,
+        name,
+        email,
+    };
+
+    USERS.push(createdUser);
+    res.status(201).json(createUser);
+};
+
+exports.createUser = getUserById;
+exports.createUser = getUserById;
