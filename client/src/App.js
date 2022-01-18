0f2e004beb2b5a40fede60474f3da27c8c92a8da
Ida Loggert <ida.loggert@gmail.com>
2022-01-18 17:06:36 +0100

    add automatic logout when token expires

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import "./App.css";
 import {
     BrowserRouter as Router,
@@ -17,21 +17,62 @@ import Navbar from "./shared/Navbar";
 import Login from "./users/Login";
 import { AuthContext } from "./utils/auth-context";
 
-function App() {
+let logoutTimer;
+
+const App = () => {
     const [token, setToken] = useState(false);
+    const [tokenExpirationDate, setTokenExpirationDate] = useState();
     const [userId, setUserId] = useState(false);
 
-    const login = useCallback((userId, token) => {
+    const login = useCallback((userId, token, expirationDate) => {
         setToken(token);
         setUserId(userId);
+        const updatedTokenExpirationDate =
+            expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+        setTokenExpirationDate(updatedTokenExpirationDate);
+        localStorage.setItem(
+            "userData",
+            JSON.stringify({
+                userId,
+                token,
+                expiration: updatedTokenExpirationDate.toISOString(),
+            })
+        );
         console.log("isloggeed in");
     }, []);
 
     const logout = useCallback(() => {
         setToken(null);
         setUserId(null);
+        setTokenExpirationDate(null);
+        localStorage.removeItem("userData");
         console.log("islogged out");
     }, []);
+
+    useEffect(() => {
+        if (token && tokenExpirationDate) {
+            const remainingTime =
+                tokenExpirationDate.getTime() - new Date().getTime();
+            logoutTimer = setTimeout(logout, remainingTime);
+        } else {
+            clearTimeout(logoutTimer);
+        }
+    }, [token, logout, tokenExpirationDate]);
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem("userData"));
+        if (
+            storedData &&
+            storedData.token &&
+            new Date(storedData.expiration) > new Date()
+        ) {
+            login(
+                storedData.userId,
+                storedData.token,
+                new Date(storedData.expiration)
+            );
+        }
+    }, [login]);
 
     let routes;
 
@@ -91,6 +132,6 @@ function App() {
             </Router>
         </AuthContext.Provider>
     );
-}
+};
 
 export default App;
