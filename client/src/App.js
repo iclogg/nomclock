d4f4e01eeb46918f92897ea5ac7cdc06f7f8448d
Ida Loggert <ida.loggert@gmail.com>
2022-01-19 10:08:51 +0100

    move auth logic from app component to separate hook file

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import {
     BrowserRouter as Router,
@@ -16,63 +16,10 @@ import Welcome from "./info/Welcome";
 import Navbar from "./shared/Navbar";
 import Login from "./users/Login";
 import { AuthContext } from "./utils/auth-context";
-
-let logoutTimer;
+import { useAuth } from "./utils/auth-hook";
 
 const App = () => {
-    const [token, setToken] = useState(false);
-    const [tokenExpirationDate, setTokenExpirationDate] = useState();
-    const [userId, setUserId] = useState(false);
-
-    const login = useCallback((userId, token, expirationDate) => {
-        setToken(token);
-        setUserId(userId);
-        const updatedTokenExpirationDate =
-            expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-        setTokenExpirationDate(updatedTokenExpirationDate);
-        localStorage.setItem(
-            "userData",
-            JSON.stringify({
-                userId,
-                token,
-                expiration: updatedTokenExpirationDate.toISOString(),
-            })
-        );
-        console.log("isloggeed in");
-    }, []);
-
-    const logout = useCallback(() => {
-        setToken(null);
-        setUserId(null);
-        setTokenExpirationDate(null);
-        localStorage.removeItem("userData");
-        console.log("islogged out");
-    }, []);
-
-    useEffect(() => {
-        if (token && tokenExpirationDate) {
-            const remainingTime =
-                tokenExpirationDate.getTime() - new Date().getTime();
-            logoutTimer = setTimeout(logout, remainingTime);
-        } else {
-            clearTimeout(logoutTimer);
-        }
-    }, [token, logout, tokenExpirationDate]);
-
-    useEffect(() => {
-        const storedData = JSON.parse(localStorage.getItem("userData"));
-        if (
-            storedData &&
-            storedData.token &&
-            new Date(storedData.expiration) > new Date()
-        ) {
-            login(
-                storedData.userId,
-                storedData.token,
-                new Date(storedData.expiration)
-            );
-        }
-    }, [login]);
+    const { token, userId, login, logout } = useAuth();
 
     let routes;
 
---
+++ client/src/utils/auth-hook.js
@@ -0,0 +1,61 @@
+import React, { useState, useCallback, useEffect } from "react";
+
+let logoutTimer;
+
+export const useAuth = () => {
+    const [token, setToken] = useState(false);
+    const [tokenExpirationDate, setTokenExpirationDate] = useState();
+    const [userId, setUserId] = useState(false);
+
+    const login = useCallback((userId, token, expirationDate) => {
+        setToken(token);
+        setUserId(userId);
+        const updatedTokenExpirationDate =
+            expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+        setTokenExpirationDate(updatedTokenExpirationDate);
+        localStorage.setItem(
+            "userData",
+            JSON.stringify({
+                userId,
+                token,
+                expiration: updatedTokenExpirationDate.toISOString(),
+            })
+        );
+        console.log("isloggeed in");
+    }, []);
+
+    const logout = useCallback(() => {
+        setToken(null);
+        setUserId(null);
+        setTokenExpirationDate(null);
+        localStorage.removeItem("userData");
+        console.log("islogged out");
+    }, []);
+
+    useEffect(() => {
+        if (token && tokenExpirationDate) {
+            const remainingTime =
+                tokenExpirationDate.getTime() - new Date().getTime();
+            logoutTimer = setTimeout(logout, remainingTime);
+        } else {
+            clearTimeout(logoutTimer);
+        }
+    }, [token, logout, tokenExpirationDate]);
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem("userData"));
+        if (
+            storedData &&
+            storedData.token &&
+            new Date(storedData.expiration) > new Date()
+        ) {
+            login(
+                storedData.userId,
+                storedData.token,
+                new Date(storedData.expiration)
+            );
+        }
+    }, [login]);
+
+    return { token, userId, login, logout };
+};
