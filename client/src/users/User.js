62792979f9073217c2bcc3185c78f2e182f98c8c
Ida Loggert <ida.loggert@gmail.com>
2022-01-28 14:59:18 +0100

    add account and pet tabs to userpage and start work on account setting commponenet

--- client/src/users/User.js
+++ client/src/users/User.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState, useContext } from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
-import PetsList from "../pets/PetsList";
 import Loading from "../shared/Loading";
 import Error from "../shared/Error";
+
+import PetsList from "../pets/PetsList";
 
 import useAxios from "../utils/axios-hook";
 import { AuthContext } from "../utils/auth-context";
@@ -29,15 +34,65 @@ const User = () => {
         getPets();
     }, [auth, sendRequest]);
 
+    // TAB LOGIC
+    const [tabValue, setTabValue] = useState(0);
+
+    function TabPanel(props) {
+        const { children, value, index } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+            >
+                {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+            </div>
+        );
+    }
+
+    const handleChange = (event, newValue) => {
+        setTabValue(newValue);
+    };
+    function a11yProps(index) {
+        return {
+            id: `simple-tab-${index}`,
+            "aria-controls": `simple-tabpanel-${index}`,
+        };
+    }
+
     return (
         <div>
             {isLoading && <Loading />}
             {error && <Error message={error} onClick={clearError} />}
-            <h1>Pet Owner Page</h1>
-            <div>
-                <h2>Your Pets</h2>
-                {!isLoading && <PetsList items={pets} isLoading />}
-            </div>
+            <Typography variant="h3" mt="10px">
+                Pet Owner Page
+            </Typography>
+            <Box
+                sx={{
+                    width: "100%",
+                }}
+            >
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                    <Tabs
+                        value={tabValue}
+                        onChange={handleChange}
+                        aria-label="account tabs"
+                        textColor={"secondary"}
+                        indicatorColor="secondary"
+                    >
+                        <Tab label="Pets" {...a11yProps(0)} />
+                        <Tab label="Account" {...a11yProps(1)} />
+                    </Tabs>
+                    <TabPanel value={tabValue} index={0}>
+                        {!isLoading && <PetsList items={pets} isLoading />}
+                    </TabPanel>
+                    <TabPanel value={tabValue} index={1}>
+                        Item Two
+                    </TabPanel>
+                </Box>
+            </Box>
         </div>
     );
 };
