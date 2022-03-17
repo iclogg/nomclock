8026754c171243ca9078b90cfd55defac52d7ae4
Ida Loggert <ida.loggert@gmail.com>
2022-03-17 17:57:46 +0100

    change colors on clock

--- client/src/clock/Clock.js
+++ client/src/clock/Clock.js
@@ -6,42 +6,6 @@ const Clock = () => {
     return (
         <div className="clockContainer">
             <div className="clock">
-                {/*       <div class="clock-number num1">
-                    <div class="">1</div>
-                </div>
-                <div class="clock-number num2">
-                    <div class="">2</div>
-                </div>
-                <div class="clock-number num3">
-                    <div class="">3</div>
-                </div>
-                <div class="clock-number num4">
-                    <div class="">4</div>
-                </div>
-                <div class="clock-number num5">
-                    <div class="">5</div>
-                </div>
-                <div class="clock-number num6">
-                    <div class="">6</div>
-                </div>
-                <div class="clock-number num7">
-                    <div class="">7</div>
-                </div>
-                <div class="clock-number num8">
-                    <div class="">8</div>
-                </div>
-                <div class="clock-number num9">
-                    <div class="">9</div>
-                </div>
-                <div class="clock-number num10">
-                    <div class="">10</div>
-                </div>
-                <div class="clock-number num11">
-                    <div class="">11</div>
-                </div>
-                <div class="clock-number num12">
-                    <div class="">12</div>
-                </div> */}
                 <div class="outer-clock-face">
                     <div class="marking marking-vertical quaterday">
                         <div className="pm">24</div>
