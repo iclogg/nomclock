a15bb0cba1e90d6122602c14a12ea0d051a6788c
Ida Loggert <ida.loggert@gmail.com>
2022-04-07 11:08:06 +0200

    add trim() to validator that need it.

--- client/src/shared/form/validators.js
+++ client/src/shared/form/validators.js
@@ -1,20 +1,22 @@
 const inputValidator = (str, value) => {
     if ("email" === str) {
-        return /(.+)@(.+){1,}\.(.+){2,}/.test(value)
+        return /(.+)@(.+){1,}\.(.+){2,}/.test(value.trim())
             ? ""
             : "Please enter a valid email.";
     }
     if ("password" === str) {
-        return value.length > 5
+        return value.trim().length > 5
             ? ""
             : "A password has minimum of 6 characters.";
     }
 
     if ("name" === str) {
-        return value.length > 0 ? "" : "Please enter your name.";
+        return value.trim().length > 0 ? "" : "Please enter your name.";
     }
     if ("description" === str) {
-        return value.length > 3 ? "" : "Please enter at least 4 characters.";
+        return value.trim().length > 3
+            ? ""
+            : "Please enter at least 4 characters.";
     }
     if ("maxMeals" === str) {
         return value > 0 ? "" : "Please enter a max amount of meals";
