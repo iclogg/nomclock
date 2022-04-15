c3c91b326df3b5d810f6eda4061ed129d30e578e
Ida Loggert <ida.loggert@gmail.com>
2022-04-15 11:22:22 +0200

    add comments for clarity

--- client/src/shared/form/Form.js
+++ client/src/shared/form/Form.js
@@ -7,24 +7,30 @@ export const useForm = ({ initialValues, validateOnChange = false }) => {
     const [inputErrors, setInputErrors] = useState({});
 
     const validate = (fieldValues = values) => {
+        // Copy current inputError state
         let temp = { ...inputErrors };
 
+        // Loop over current fieldvalues update temp error obj accordingly with the inputvalidator
         for (const key in fieldValues) {
             if (key in fieldValues) {
                 temp[key] = inputValidator(key, fieldValues[key]);
             }
         }
 
+        // Update the current inputErrors state
         setInputErrors({ ...temp });
 
+        // Return true or false by checking all errors are empty strings or not
         return Object.values(temp).every((x) => x == "");
     };
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
 
+        // Set new value
         setValues({ ...values, [name]: value });
 
+        // Set error status if needed
         if (validateOnChange) {
             validate({ [name]: value });
         }
@@ -47,7 +53,8 @@ export const Form = (props) => {
     return (
         <form
             style={formStyle}
-            {...other} /* TODO: uncomment this when not needed for smother login when developing: autoComplete="off" */
+            {...other}
+            /* TODO: uncomment this when not needed for smother login when developing: autoComplete="off" */
         >
             {children}
         </form>
