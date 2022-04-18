2f59174368e31da93e34e486f285d9682f2d195c
Ida Loggert <ida.loggert@gmail.com>
2022-04-18 15:28:02 +0200

    updating styling

--- client/src/users/AuthGrid.js
+++ client/src/users/AuthGrid.js
@@ -4,8 +4,17 @@ const AuthGrid = (props) => {
     return (
         <Grid
             container
-            spacing={2}
-            mt={10} /* sx={{ border: "black solid 2px" }} */
+            height="100vh"
+            alignItems="flex-end"
+            justifyContent="center"
+            /* sx={{ border: "black solid 2px" }} */
+            sx={{
+                backgroundImage:
+                    "url(https://images.unsplash.com/photo-1625316708582-7c38734be31d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80)",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+            }}
         >
             {props.children}
         </Grid>
